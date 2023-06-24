7c631a4af199b1f0113d3498632ea75e9cc6c761
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-06-24 20:12:31 +0300

    first commit

---
+++ src/js/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { currentPage, perPage } from './index.js';
+
+const API_KEY = '37352949-d51cc3913c6f19a152f1908cc';
+axios.defaults.baseURL = 'https://pixabay.com/api/'
+
+export async function getData(searchQuery) {
+    try {
+        const res = await axios.get(getRequestUrl(searchQuery));
+        return res.data;
+    } catch(error) {
+        throw Error(error.response?.statusText || error.message);
+    }
+}
+
+export function getRequestUrl(searchQuery) {
+    const params = {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: currentPage,
+        per_page: perPage
+    }
+    const queryString = new URLSearchParams(params).toString();
+    return `?${queryString}`;
+}
